9c4ee4227e7b3758d6664ef41b457ed505c37382
Tamim Hasan <66991901+tamimhasan404@users.noreply.github.com>
2022-03-26 23:12:48 +0600

    Create gau-expose.sh

---
+++ gau-expose.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+
+echo -e '\033[1;38;5;221m'"
+
++-+-+-+ +-+-+-+-+-+-+
+|G|a|u| |E|x|p|o|s|e|
++-+-+-+ +-+-+-+-+-+-+ 
+Author:Tamim Hasan(tamimhasan404)"
+echo
+
+mkdir gau-expose
+
+echo -n "[] Give your gau file name/path: "
+
+read path
+
+echo
+
+echo "[] Greather senstive file"
+
+echo
+
+cp $path gau-expose
+cd gau-expose
+
+cat $path | grep ".xls\|.xlsx\|.sql\|.csv\|.env\|.msql\|.bak\|.bkp\|.bkf\|.old\|.temp\|.db\|.mdb\|.config\|.yaml\|.zip\|.tar\|.git\|.xz\|.asmx\|.vcf\|.pem" | uro | sort | uniq  > gau-sensitive-file.txt
+
+echo "[] Greather all panel stuff"
+
+cat $path | grep -i "login\|singup\|admin\|dashboard\|wp-admin\|singin\|adminer\|dana-na\|login/?next/=" | sort | uniq | uro > gau-panel.txt
+echo
+echo "[] Greather third-party assets"
+
+cat $path | grep -i "jira\|jenkins\|grafana\|mailman\|+CSCOE+\|+CSCOT+\|+CSCOCA+\|symfony\|debug\|gitlab\|phpmyadmin\|phpMyAdmin" | sort | uniq | uro > gau-third-party-assets.txt
+
+echo
+echo "[] Greathering emails-usersnames" 
+cat $path | grep "@" | sort | uniq | uro > gau-emails-usersnames.txt
+
+echo
+echo "[] Greathering error(may sensitive-data-expose)"
+cat $path | grep "error." | sort | uniq | uro > gau-error-base.txt
+
+echo
+echo "[] Grathering other sensitive path"
+cat $path | grep -i "root\| internal\| private\|secret" | sort | uniq | uro > other-sensitive-path.txt
+
+echo
+echo "[] Grathering only robots.txt"
+cat $path | grep -i robots.txt | sort | uniq | uro > only-robots.txt
+
+echo
+echo "[] Grathering subdomains"
+cat $path | cut -d'/' -f3 | cut -d':' -f1 | uro | sed 's/^\(\|s\):\/\///g' > subdomains.txt
+
+echo
+echo "[] Grathering paths for directory brute-force"
+
+cat $path | rev | cut -d '/' -f 1 | rev | uro | sed 's/^\(\|s\):\/\///g' | sed '/=\|.js\|.gif\|.css\|.jpeg\|.png\|:\|%/d' > wordlist.txt
+
+rm $path
+
+echo
+
+echo -e "\e[1mDone, Hope it's helpful for you\e[0m"
